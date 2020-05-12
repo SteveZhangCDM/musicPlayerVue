d59fe195dcc604b31962c264003cf7d57f4da7ba
Steve_Zhang <wentaozhang0305@gmail.com>
2020-05-11 20:56:23 -0700

    set up html

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
+    query: '',
+    musicList: [],
+  },
+  methods: {
+    searchMusic: function () {
+      axios.get('https://autumnfish.cn/search?keywords=' + this.query).then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {
+          console.log(err);
+        }
+      );
+    },
   },
 });
