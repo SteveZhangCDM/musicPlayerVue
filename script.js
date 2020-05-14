daa4f8d30e479ecf09eef2d928c7b5002b3e6517
Steve_Zhang <wentaozhang0305@gmail.com>
2020-05-13 20:53:11 -0700

    set up audio player in html and got url when click icon before the list

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var app = new Vue({
   data: {
     query: '',
     musicList: [],
+    musicUrl: '',
   },
   methods: {
     searchMusic: function () {
@@ -10,6 +11,21 @@ var app = new Vue({
       axios.get('https://autumnfish.cn/search?keywords=' + this.query).then(
         function (response) {
           that.musicList = response.data.result.songs;
+        },
+        function (err) {
+          console.log(err);
+        }
+      );
+    },
+    playMusic: function (musicId) {
+      event.preventDefault();
+      console.log(musicId);
+      let that = this;
+      axios.get('https://autumnfish.cn/song/url?id=' + musicId).then(
+        function (response) {
+          console.log(response);
+          console.log(response.data.data[0].url);
+          that.musicUrl = response.data.data[0].url;
         },
         function (err) {
           console.log(err);
